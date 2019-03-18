e8025b426ffd18723cece1b57abf78752e1f83d2
Leonardo Job <44514091+congarr@users.noreply.github.com>
2019-03-17 22:46:54 -0300

    a
    a

---
+++ site html-printo/newsletter.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title> _cyberpunk2077.wiki</title>
+    <link rel="stylesheet" type="text/css"href="css/style.css">
+    
+  </head>
+
+  <body>
+    
+    <header>
+      </div class=indexpage>
+      <a href="index.html"> <img src="img\cyberpunk_logo.png" style="width: 50%"> </a>
+      </div>
+
+      <div class=content>
+         Hi <?php echo htmlspecialchars($_POST['name']); ?>
+      </div>
+    </main>
+  </body>
+</html>
+
